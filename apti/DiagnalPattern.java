4087c500c3c904ab0de169131c1ec97b184bbd25
ThrishulKD <kdthrishul@gmail.com>
2021-11-13 17:05:51 +0530

    Apti Patterns

---
+++ apti/DiagnalPattern.java
@@ -0,0 +1,16 @@
+class DiagnalPattern{
+	
+	public static void main(String[] arg){
+		
+	for(int row =1;row<=5;row++){
+		System.out.print("\n");
+		for(int col=1;col<=5;col++){
+			
+			System.out.print(row+""+col+"\t");
+			
+		}
+//		System.out.println(row);
+	}
+		
+	}
+}
